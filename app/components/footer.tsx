e5b47136f628d7d5a2e7696a07a9839ddbd4817b
laamvr <laamvr@gmail.com>
2026-06-11 11:47:44 -0500

    random fixes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,17 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 function ArrowIcon() {
   return (
     <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      width='12'
+      height='12'
+      viewBox='0 0 12 12'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
     >
       <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
+        d='M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z'
+        fill='currentColor'
       />
     </svg>
   );
@@ -19,45 +19,45 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 text-sm">
+    <footer className='mb-16'>
+      <ul className='font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 text-sm'>
         <li>
           <Link
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
+            className='flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100'
+            rel='noopener noreferrer'
+            target='_blank'
+            href='/rss'
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className='ml-2 h-7'>rss</p>
           </Link>
         </li>
         <li>
           <Link
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/laamvr"
+            className='flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100'
+            rel='noopener noreferrer'
+            target='_blank'
+            href='https://github.com/laamvr'
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className='ml-2 h-7'>github</p>
           </Link>
         </li>
         <li>
           <Link
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://youtube.com/@laamvr"
+            className='flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100'
+            rel='noopener noreferrer'
+            target='_blank'
+            href='https://netisen.com'
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">youtube</p>
+            <p className='ml-2 h-7'>netisen</p>
           </Link>
         </li>
       </ul>
-      {/* <p className="mt-8 text-neutral-600 dark:text-neutral-300 text-sm">
+      <p className='mt-8 text-neutral-600 dark:text-neutral-300 text-sm'>
         © {new Date().getFullYear()} Victor Laam. All rights reserved.
-      </p> */}
+      </p>
     </footer>
   );
 }
